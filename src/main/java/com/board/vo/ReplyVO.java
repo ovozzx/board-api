4bb39f2601c937d691474da6a788b233744afb79
ovozzx <takiwow19@gmail.com>
2026-03-07 05:54:27 +0900

    [feat] 답글 4계층 기능 추가

--- src/main/java/com/board/vo/ReplyVO.java
+++ src/main/java/com/board/vo/ReplyVO.java
@@ -1,5 +1,8 @@
 package com.board.vo;
 
+import lombok.Data;
+
+@Data
 public class ReplyVO {
 
 	private int replyId;        // REPLY_ID
@@ -10,55 +13,7 @@ public class ReplyVO {
     private String createDate; // CREATE_DATE
     private String modifyDate; // MODIFY_DATE
     private String useYn;        // USE_YN
-    
-	public int getReplyId() {
-		return replyId;
-	}
-	public void setReplyId(int replyId) {
-		this.replyId = replyId;
-	}
-	public int getBoardId() {
-		return boardId;
-	}
-	public void setBoardId(int boardId) {
-		this.boardId = boardId;
-	}
-	public Integer getParentReplyId() {
-		return parentReplyId;
-	}
-	public void setParentReplyId(Integer parentReplyId) {
-		this.parentReplyId = parentReplyId;
-	}
-	public String getCreateUser() {
-		return createUser;
-	}
-	public void setCreateUser(String createUser) {
-		this.createUser = createUser;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getCreateDate() {
-		return createDate;
-	}
-	public void setCreateDate(String createDate) {
-		this.createDate = createDate;
-	}
-	public String getModifyDate() {
-		return modifyDate;
-	}
-	public void setModifyDate(String modifyDate) {
-		this.modifyDate = modifyDate;
-	}
-	public String getUseYn() {
-		return useYn;
-	}
-	public void setUseYn(String useYn) {
-		this.useYn = useYn;
-	}
+	private int depth;
+	private String path;
 
-    
 }
